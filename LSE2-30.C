36f58b27470df912fd6270787e92364bdc6ed689
Lavish Swarnkar <lavishswarnkar@gmail.com>
2017-08-24 10:18:01 +0530

    Update LSE2-30.C

--- LSE2-30.C
+++ LSE2-30.C
@@ -8,15 +8,15 @@ void main()
      printf("Enter a character to check if it is vowel or not (Plz enter A-Z or a-z only): ");
      scanf("%c",&alpha);
      switch(alpha)
-     {	  case 'a': printf("It's a vowel"); break;
-	  case 'e': printf("It's a vowel"); break;
-	  case 'i': printf("It's a vowel"); break;
-	  case 'o': printf("It's a vowel"); break;
-	  case 'u': printf("It's a vowel"); break;
-	  case 'A': printf("It's a vowel"); break;
-	  case 'E': printf("It's a vowel"); break;
-	  case 'I': printf("It's a vowel"); break;
-	  case 'O': printf("It's a vowel"); break;
+     {	  case 'a': 
+	  case 'e': 
+	  case 'i': 
+	  case 'o': 
+	  case 'u': 
+	  case 'A': 
+	  case 'E': 
+	  case 'I': 
+	  case 'O': 
 	  case 'U': printf("It's a vowel"); break;
 	  default: printf("It's a consonant");
      }
