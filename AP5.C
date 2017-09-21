05c37f6287e9f29ae31ad0c48b1b9f3e36d3104e
Lavish Swarnkar <lavishswarnkar@gmail.com>
2017-09-21 16:33:47 +0530

    Create AP5.C

---
+++ AP5.C
@@ -0,0 +1,12 @@
+//Made by Lavish Swarnkar
+//Predict the Output
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    int x=2,y=2;
+     clrscr();
+     x=y+1,y+2,y+3;
+     y=(x+1,x+2,x+3);
+     printf("x=%d y=%d",x,y);
+     getch();
+}
