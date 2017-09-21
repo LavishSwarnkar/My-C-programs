19a07e5dc987c46ee7924661077576b8bd67c3a1
Lavish Swarnkar <lavishswarnkar@gmail.com>
2017-09-21 16:48:04 +0530

    Create AP6.C

---
+++ AP6.C
@@ -0,0 +1,12 @@
+//Made by Lavish Swarnkar
+//Predict the Output
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    int x=2,y=(2,2,3);
+     clrscr();
+     2,3;
+     4,5;
+     printf("x=%d y=%d",x,y);
+     getch();
+}
