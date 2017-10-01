155aa234ae303ae57adff9d28008852228ae53bd
Lavish Swarnkar <lavishswarnkar@gmail.com>
2017-10-01 14:31:01 +0530

    Update LSE2-47.C

--- LSE2-47.C
+++ LSE2-47.C
@@ -2,11 +2,8 @@
 //Functions (add two numbers-2.0)
 #include<stdio.h>
 #include<conio.h>
-void add(int n1, int n2)
-{       int s;
-	s=n1+n2;
-	printf("\n%d + %d = %d",n1,n2,s);
-}
+void add(int n1, int n2);
+
 void main()
 {    int n1,n2;
      clrscr();
@@ -14,4 +11,9 @@ void main()
      scanf("%d %d",&n1,&n2);
      add(n1,n2);
      getch();
+}
+void add(int n1, int n2)
+{       int s;
+	s=n1+n2;
+	printf("\n%d + %d = %d",n1,n2,s);
 }
