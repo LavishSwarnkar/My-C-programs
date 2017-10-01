994410eb68d33db294c31471975c9dee135b0708
Lavish Swarnkar <lavishswarnkar@gmail.com>
2017-10-01 16:42:05 +0530

    Add files via upload

---
+++ LSE2-51.C
@@ -0,0 +1,19 @@
+//LSE2-51
+//Functions (Swapping by pass by value)
+#include<stdio.h>
+#include<conio.h>
+void swap(int x,int y)
+{	   printf("Before swapping, x=%d and y=%d",x,y);
+     x=x+y,y=x-y,x=x-y;
+     printf("\nAfter swapping in function, x=%d and y=%d",x,y);
+}
+void main()
+{    int x,y;
+     clrscr();
+     printf("Enter two numbers: ");
+     scanf("%d %d",&x,&y);
+     swap(x,y);
+     printf("\nAfter returning from function, x=%d and y=%d",x,y);
+     printf("\nNote that Values remain unchanged in main()");
+	    getch();
+}
