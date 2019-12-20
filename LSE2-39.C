d862ee820403877365dbbc3658677742ea4b1230
Swapnil bhojwani <53923373+Swapnil7000@users.noreply.github.com>
2019-12-20 22:42:09 +0530

    Update LSE2-39.C

--- LSE2-39.C
+++ LSE2-39.C
@@ -8,7 +8,7 @@ void main()
      clrscr();
      printf("Enter year: ");
      scanf("%d",&year);
-     if(year%400==0||year%100==0||year%4==0)
+     if((year%400==0)||(year%100!=0&&year%4==0))
 	printf("The year %d is a leap year",year);
      else
 	printf("The year %d is not a leap year",year);
